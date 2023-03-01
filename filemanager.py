c8263ad2b39286391d2a4c7e41b9f3809e03677e
relhe <renellherisson@gmail.com>
2023-02-28 20:41:45 -0500

    listing last ten files methods

--- filemanager.py
+++ filemanager.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 class FileManager:
     def __init__(self, path = "./"):
@@ -20,3 +21,27 @@ class FileManager:
         self.files.sort()
         for filename in self.files:
             print(filename)
+
+    def reverse_sort_all_files(self):
+        self.files.sort()
+        self.files.reverse()
+        for filename in self.files:
+            print(filename)
+
+    def list_first_ten_files(self):
+        if len(self.files) > 10:
+            files = self.files[0:10]
+            for filename in files:
+                print(filename)
+        else:
+            for filename in self.files:
+                print(filename)
+
+    def list_last_ten_files(self):
+        if len(self.files) > 10:
+            files = self.files[-10:]
+            for filename in files:
+                print(filename)
+        else:
+            for filename in self.files:
+                print(filename)
